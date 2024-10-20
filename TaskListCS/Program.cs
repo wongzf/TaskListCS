3a8264fa4cd8f391bcfcee0ebdfa7b199a93ec0f
wongzf <wongzf@outlook.com>
2024-10-21 01:07:26 +0800

    Added "Display All Task" functionality
    Need further formatting to make it look nice. Consider adding text limit for formatting purpose.

--- TaskListCS/Program.cs
+++ TaskListCS/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ public class Program
         while (isRunning) 
         {
             //Print Instruction lines
-            Console.WriteLine("WZF Task List");
+            Console.WriteLine("\nWZF Task List");
             Console.WriteLine("=========================================");
             Console.WriteLine("Input number to choose option: ");
             Console.WriteLine("1. Add Task");
@@ -36,6 +37,7 @@ public class Program
 
                 case "2" :
                     Console.WriteLine("Display Task Selected");
+                    DisplayAllTasks();
                     break;
 
                 case "3" :
@@ -76,15 +78,54 @@ public class Program
         
         Task newTask = new Task(taskName, dueDate, isCompleted);
         TaskList.Add(newTask);
-        Console.WriteLine("Task added successfully!");
+        Console.WriteLine("Task added successfully! Returning to Main Menu\n");
+    }
+
+    private static void DisplayAllTasks()
+    {
+        // If there are no task, Show "No task Available"
+        if (TaskList.Count == 0)
+        {
+            Console.WriteLine("No tasks available.");
+        }
+        else
+        {
+            // Table Header, needs further formatting to look nice
+            Console.WriteLine("\nAll Tasks || Due Date || Status");
+            
+            //Loop thru TaskList list to cout all task
+            for (int i = 0; i < TaskList.Count; i++)
+            {
+                string TaskText = TaskList[i].TaskText;
+                DateTime DueDate = TaskList[i].DueDate;
+                string IsCompletedText = "Incomplete";
+
+                //Set Completed text instead of just True/False
+                if (TaskList[i].IsCompleted)
+                {
+                    IsCompletedText = "Completed!";
+                }
+                else 
+                {
+                    IsCompletedText = "Incomplete!";
+                }
+
+                Console.WriteLine(TaskText + " || " + DueDate + " || " + IsCompletedText);
+            }
+        }
     }
 }
 
-public class Task 
+public class Task
 {
-    private string TaskText; // Task Name/Text
-    private DateTime DueDate; // When is the task due? 
-    private bool IsCompleted; // Is the task completed?
+    private string tasktext;
+    public string TaskText { get; set; } // Task Name/Text
+
+    private DateTime duedate;
+    public DateTime DueDate { get; set; } // When is the task due? 
+
+    private bool iscompleted;
+    public bool IsCompleted { get; set; } // Is the task completed?
 
     // Constructor for Task
     public Task(string taskText, DateTime dueDate, bool isCompleted)
